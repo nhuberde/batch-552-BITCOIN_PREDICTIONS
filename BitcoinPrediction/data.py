741862dc1aa32d7216c67c5533e902247cffac13
Anne Constance six <ac.six@skema.edu>
2021-02-26 12:03:26 +0100

    added amelie's fonction to create train_test_split

--- BitcoinPrediction/data.py
+++ BitcoinPrediction/data.py
@@ -3,31 +3,32 @@ import pandas as pd
 
 def get_data():
     """method to get the training data (or a portion of it) from google cloud bucket"""
-    client = storage.Client()
+    # client = storage.Client()
     # data = pd.read_csv(f"gs://{BUCKET_NAME}/{BUCKET_TRAIN_DATA_PATH}")
-    # data = pd.read_csv('../raw_data/bitstampUSD.csv')
-    data = pd.read_csv('gs://bitcoin-prediction-01/data/bitstampUSD.csv')
-    return data
-
-
-def clean_data(data, test=False):
-    data = data.copy()
+    data = pd.read_csv('../raw_data/bitstampUSD.csv')
+    # data = pd.read_csv('gs://bitcoin-prediction-01/data/bitstampUSD.csv')
     data = data[2798176:4727776]
-    data = data.drop(columns=['High', 'Low', 'Close', 'Volume_(BTC)', 'Volume_(Currency)', 'Weighted_Price'])
     data['Timestamp'] = pd.to_datetime(data['Timestamp'], unit='s', origin='unix')
-    data_test = data_sample[["Timestamp", "Open"]]
-    data = data.fillna(method='ffill')
+    data = data[['Open', 'Timestamp']].set_index("Timestamp").fillna(method='ffill')
+    data = data.dropna()
     return data
 
-def open_diff_col(data):
-    data['Open_diff'] = data["Open"].diff()
-    diff_data = data[1:]
-    return diff_data
 
-def y_encoding(data):
-    data['Coded'] = data['Open_diff'].map(lambda x: 0 if x <= 0 else 1)
-    return data
+def preprocessing_data(data, shift_size, h=1):
+    
+    data_pp = data.copy()    
+    data_pp['diff_Open'] = data_pp['Open'].diff(h)
+    data_pp['diff_Open'] = data_pp['diff_Open'].dropna()
+    
+    data_pp[f"t+{h}"]= data_pp['diff_Open'].shift(-h)
 
-if __name__ == '__main__':
-    data = get_data()
-    print(data.shape)
+    for i in range(0, shift_size):
+        data_pp[f't-{i}'] = data_pp['Open'].shift(i)
+    data_shifted = data_pp.dropna()
+    
+    X = data_shifted.drop(columns=['Open', 'diff_Open', f"t+{h}"])
+    y = data_shifted[f"t+{h}"]
+    y[y > 0] = 1
+    y[y <= 0] = 0
+
+    return X, y, data_shifted
--- BitcoinPrediction/train_test_split.py
+++ BitcoinPrediction/train_test_split.py
@@ -1,14 +1,23 @@
-def train_test_split(data, sample_size, shift_size, train_size):
-    data_size = data.shape[0]
-    sample = data.iloc[(data_size-sample_size):data_size]
-    sample_pp = sample[['Open', 'Timestamp']].set_index("Timestamp").fillna(method='backfill')
-    for i in range(1, shift_size+1):
-        sample_pp[f't - {i}'] = sample_pp['Open'].shift(i)
-    sample_shifted = sample_pp.dropna() 
-    X = sample_shifted.drop(columns=['Open'])
-    y = sample_shifted['Open']
-    X_train = X.iloc[0:train_size]
-    y_train = y.iloc[0:train_size]
-    X_test = X.iloc[(train_size+1):(sample_size-shift_size)]
-    y_test = y.iloc[(train_size+1):(sample_size-shift_size)]
+from BitcoinPrediction.data import get_data, preprocessing_data
+
+
+def input_data(data, sample_size, shift_size, train_size, h=1, w=0):
+    
+    X, y, data_shifted = preprocessing_data(data, shift_size, h)
+    
+    data_size = data_shifted.shape[0]
+    sample_X = data_shifted.iloc[(data_size-sample_size-w):data_size-w]
+    sample_y = data_shifted.iloc[(data_size-sample_size-w):data_size-w]
+
+    X_train = sample_X.iloc[0:train_size]
+    y_train = sample_y.iloc[0:train_size] 
+    
+    X_test = sample_X.iloc[(train_size+h-1):(sample_size-shift_size)]
+    y_test = sample_y.iloc[(train_size+h-1):(sample_size-shift_size)] 
+    
+    
     return X_train, X_test, y_train, y_test
+
+if __name__ == '__main__':
+    X_train, X_test, y_train, y_test = input_data()
+    print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
