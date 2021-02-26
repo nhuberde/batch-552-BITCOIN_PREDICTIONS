5bd9d733e826422775b35fdf8d6e21e31846ca7f
Anne Constance six <ac.six@skema.edu>
2021-02-26 14:47:49 +0100

    changes to get the good train test plit dataframe

--- BitcoinPrediction/data.py
+++ BitcoinPrediction/data.py
@@ -7,28 +7,22 @@ def get_data():
     # data = pd.read_csv(f"gs://{BUCKET_NAME}/{BUCKET_TRAIN_DATA_PATH}")
     data = pd.read_csv('../raw_data/bitstampUSD.csv')
     # data = pd.read_csv('gs://bitcoin-prediction-01/data/bitstampUSD.csv')
-    data = data[2798176:4727776]
-    data['Timestamp'] = pd.to_datetime(data['Timestamp'], unit='s', origin='unix')
-    data = data[['Open', 'Timestamp']].set_index("Timestamp").fillna(method='ffill')
-    data = data.dropna()
     return data
 
 
 def preprocessing_data(data, shift_size, h=1):
-    
-    data_pp = data.copy()    
+    data_pp = data[2798176:4727776]
+    data_pp['Timestamp'] = pd.to_datetime(data_pp['Timestamp'], unit='s', origin='unix')
+    data_pp = data_pp[['Open', 'Timestamp']].set_index("Timestamp").fillna(method='ffill')
+    data_pp
     data_pp['diff_Open'] = data_pp['Open'].diff(h)
     data_pp['diff_Open'] = data_pp['diff_Open'].dropna()
-    
-    data_pp[f"t+{h}"]= data_pp['diff_Open'].shift(-h)
-
+    data_pp[f"t+{h}"] = data_pp['diff_Open'].shift(-h)
     for i in range(0, shift_size):
         data_pp[f't-{i}'] = data_pp['Open'].shift(i)
     data_shifted = data_pp.dropna()
-    
     X = data_shifted.drop(columns=['Open', 'diff_Open', f"t+{h}"])
     y = data_shifted[f"t+{h}"]
     y[y > 0] = 1
     y[y <= 0] = 0
-
     return X, y, data_shifted
--- BitcoinPrediction/train_test_split.py
+++ BitcoinPrediction/train_test_split.py
@@ -2,22 +2,16 @@ from BitcoinPrediction.data import get_data, preprocessing_data
 
 
 def input_data(data, sample_size, shift_size, train_size, h=1, w=0):
-    
     X, y, data_shifted = preprocessing_data(data, shift_size, h)
-    
     data_size = data_shifted.shape[0]
-    sample_X = data_shifted.iloc[(data_size-sample_size-w):data_size-w]
-    sample_y = data_shifted.iloc[(data_size-sample_size-w):data_size-w]
-
+    sample_X = X.iloc[(data_size-sample_size-w):data_size-w]
+    sample_y = y.iloc[(data_size-sample_size-w):data_size-w]
     X_train = sample_X.iloc[0:train_size]
-    y_train = sample_y.iloc[0:train_size] 
-    
+    y_train = sample_y.iloc[0:train_size]
     X_test = sample_X.iloc[(train_size+h-1):(sample_size-shift_size)]
-    y_test = sample_y.iloc[(train_size+h-1):(sample_size-shift_size)] 
-    
-    
+    y_test = sample_y.iloc[(train_size+h-1):(sample_size-shift_size)]
     return X_train, X_test, y_train, y_test
 
 if __name__ == '__main__':
-    X_train, X_test, y_train, y_test = input_data()
+    X_train, X_test, y_train, y_test = input_data(data, 1000, 5, 700, h=2, w=0)
     print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
