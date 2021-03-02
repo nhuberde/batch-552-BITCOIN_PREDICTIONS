ccdf01c4fe69d17654938b6bd936e03b59736c71
Anne Constance six <ac.six@skema.edu>
2021-03-02 17:21:21 +0100

    code visual basic au top du moulin + mlflow flowing des paillettes

--- BitcoinPrediction/models.py
+++ BitcoinPrediction/models.py
@@ -2,6 +2,7 @@ from BitcoinPrediction.data import get_data
 from BitcoinPrediction.preprocessing import preprocessing_data, features_target
 from BitcoinPrediction.train_test_split import input_data
 from BitcoinPrediction.utils import select_date
+import numpy as np
 # from sklearn.linear_model import LogisticRegression, RidgeClassifier
 # from sklearn.ensemble import RandomForestClassifier
 import statistics as stats
@@ -16,7 +17,7 @@ def predict_score(model_init, X_train, X_test, y_train, y_test):
     model = model.fit(X_train, y_train)
     results = model.predict(X_test)
     score = model.score(X_test, y_test) 
-    return score, model.get_params()
+    return score, model.coef_
 
 # Cross-val function to predict the average score of our Baseline model :
 def cross_val(data, model_init=None,sample_size=1000, train_fraction=0.7, features_size=60, h=1, date_start=None, date_end=None):
@@ -33,19 +34,19 @@ def cross_val(data, model_init=None,sample_size=1000, train_fraction=0.7, featur
     reversed_intervals = reversed(intervals)
     results = []
     parameters = []
-
+    
     for i in reversed_intervals:
         X_train, X_test, y_train, y_test = input_data(X, y, sample_size, data_size, train_size, test_size, h, w=i)
         score, params = predict_score(model_init, X_train, X_test, y_train, y_test)
         results.append(score)
         parameters.append(params)
+
     
-    return results, parameters
-    # return dict({'mean_score':round(stats.mean(results),2),
-    #              'std':round(stats.stdev(results),2),
-    #              'score_min':round(min(results),2),
-    #              'score_max':round(max(results),2),
-    #              'n_fold':r})
+    return dict({'mean_score':np.around(np.mean(results), 2),
+                 'std':np.around(np.std(results), 2),   
+                 'score_min':np.around(np.amin(results), 2),
+                 'score_max':np.around(np.amax(results), 2), 
+                 'n_fold':r}), np.around(np.mean(parameters, axis=0), 4)
 
 # if __name__ == '__main__':
 #     results = run_model(data)
